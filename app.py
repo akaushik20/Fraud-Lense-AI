e05b45fa84378b821bc5341ce3bad7edb1fe73b5
Arpita Kaushik <arpita.k20@gmail.com>
2026-07-04 11:15:30 -0700

    commit

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import gradio as gr
 
+def sample_func():
+    return "My First app in Gradio AI!"
+
 if __name__=="__main__":
     demo = gr.Interface(
-    inputs="text",
-    outputs="text",
-    title="Greeter App",
-    description="Type your name below to get a greeting."
-    )
+        fn=sample_func,
+        inputs=None,
+        outputs="text",
+        title="My First App with Gradio",
+        description="Welcome to my Gradio app."
+        )
     demo.launch()
